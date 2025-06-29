a2095e29a4dd1f31b5f03c4b97efdd95c5fc5095
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-06-29 09:32:32 +0100

    class Player to Record

--- src/main/java/software/ulpgc/kata6/model/Player.java
+++ src/main/java/software/ulpgc/kata6/model/Player.java
@@ -1,45 +1,12 @@
 package software.ulpgc.kata6.model;
 
-public class Player {
-    private final String name;
-    private final String nacionality;
-    private final String position;
-    private final String squad;
-    private final League league;
-
-    public Player(String name, String nacionality, String position, String squad, League league) {
-        this.name = name;
-        this.nacionality = nacionality;
-        this.position = position;
-        this.squad = squad;
-        this.league = league;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getNacionality() {
-        return nacionality;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public String getSquad() {
-        return squad;
-    }
-
-    public League getLeague() {
-        return league;
-    }
+public record Player(String name, String nationality, String position, String squad, League league) {
 
     @Override
     public String toString() {
         return "Player{" +
                 "name='" + name + '\'' +
-                ", nacionality='" + nacionality + '\'' +
+                ", nationality='" + nationality + '\'' +
                 ", position='" + position + '\'' +
                 ", squad='" + squad + '\'' +
                 ", league=" + league +
